fdc2a4ca9230fea20f6d5e5f868ccaf1a2218de6
IsmaelG <ismaelgn89@gmail.com>
2026-06-10 12:07:01 +0200

    feat: add menu route and view

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex flex-col items-center justify-center min-h-screen gap-8">
+        <h1 class="text-yellow-400 text-4xl font-bold">
+            Battle Odyssey
+        </h1>
+
+        <a href="#" class="text-yellow-400 border-2 border-yellow-400 px-8 py-3 text-lg font-bold hover:bg-yellow-400 hover:text-blue-950 transition">
+            Start
+        </a>
+    </div>
+@endsection
