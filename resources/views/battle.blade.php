4ad02b1ba3939620c800356f4692efac7e0e08c2
IsmaelG <ismaelgn89@gmail.com>
2026-06-10 19:15:34 +0200

    Add images to battle view

--- resources/views/battle.blade.php
+++ resources/views/battle.blade.php
@@ -1,8 +1,12 @@
 <x-layouts.app>
     <div class="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center gap-8" 
-         style="background-image: url('{{ asset('images/background-wood.jpg') }}')">
-        <h2 class="text-yellow-400 text-3xl font-bold">
-            Battle
-        </h2>
+         style="background-image: url('{{ asset('images/background-wood.png') }}')">
+
+         <img src="{{ asset('images/warrior.png') }}" alt="Warrior" 
+             class="absolute bottom-64 left-60 w-96 h-96 object-contain">
+
+        <img src="{{ asset('images/goblin.png') }}" alt="Goblin" 
+             class="absolute bottom-64 right-40 w-96 h-96 object-contain">
+
     </div>
 </x-layouts.app>
