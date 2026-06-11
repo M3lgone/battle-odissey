e591e8b34d825cd1c1a9e098d8c80dc637b0ef44
IsmaelG <ismaelgn89@gmail.com>
2026-06-11 13:15:20 +0200

    feat: add logo to menu and update battle and character select views

--- resources/views/livewire/battle.blade.php
+++ resources/views/livewire/battle.blade.php
@@ -1,10 +1,40 @@
-<div class="min-h-screen bg-cover bg-center bg-no-repeat relative" 
-     style="background-image: url({{ asset('images/background-wood.png') }})">
-    
-    <img src="{{ asset('images/warrior.png') }}" alt="Warrior" 
-         class="absolute bottom-64 left-60 w-96 h-96 object-contain">
+<div class="h-screen flex flex-col">
 
-    <img src="{{ asset('images/goblin.png') }}" alt="Goblin" 
-         class="absolute bottom-64 right-40 w-96 h-96 object-contain">
+    <div class="flex-1 bg-cover bg-center bg-no-repeat relative" 
+        style="background-image: url({{ asset('images/background-wood.png') }})">
+        
+        <img src="{{ asset('images/warrior.png') }}" alt="Warrior" 
+            class="absolute bottom-64 left-60 w-96 h-96 object-contain">
 
+        <img src="{{ asset('images/goblin.png') }}" alt="Goblin" 
+            class="absolute bottom-64 right-40 w-96 h-96 object-contain">
+
+    </div>
+
+    <div class="bg-blue-950 border-t-2 border-yellow-400 grid grid-cols-4 gap-4 p-4">
+        
+        <div class="border-2 border-yellow-400 p-4 flex flex-col gap-3">
+            <h3 class="text-yellow-400 font-bold">Actions</h3>
+            <button class="text-yellow-400 border border-yellow-400 py-2 hover:bg-yellow-400 hover:text-blue-950 transition">
+                Attack
+            </button>
+        </div>
+
+        <div class="border-2 border-yellow-400 p-4">
+            <h3 class="text-yellow-400 font-bold mb-2">{{ ucfirst($character->class) }}</h3>
+            <p class="text-yellow-400">HP: {{ $character->max_health_points }}</p>
+            <p class="text-yellow-400">MP: {{ $character->max_magic_points }}</p>
+        </div>
+
+        <div class="border-2 border-yellow-400 p-4">
+            <h3 class="text-yellow-400 font-bold mb-2">Log</h3>
+        </div>
+
+        <div class="border-2 border-yellow-400 p-4">
+            <h3 class="text-yellow-400 font-bold mb-2">{{ $enemy->enemy_name }}</h3>
+            <p class="text-yellow-400">HP: {{ $enemy->max_health_points }}</p>
+            <p class="text-yellow-400">MP: {{ $enemy->max_magic_points }}</p>
+        </div>
+
+    </div>
 </div>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,10 +1,8 @@
 <x-layouts.app>
-    <div class="flex flex-col items-center justify-center min-h-screen gap-8">
-        <h1 class="text-yellow-400 text-4xl font-bold">
-            Battle Odyssey
-        </h1>
+    <div class="flex flex-col items-center justify-center min-h-screen gap-64">
+        <img src="{{ asset('images/logo-battle-odissey.png') }}" alt="Battle Odyssey" class="w-[800px]">
 
-        <a href="{{ route('character.select') }}" class="text-yellow-400 border-2 border-yellow-400 px-8 py-3 text-lg font-bold hover:bg-yellow-400 hover:text-blue-950 transition">
+        <a href="{{ route('character.select') }}" class="bg-gradient-to-b from-yellow-300 to-red-900 text-blue-950 px-8 py-3 text-lg font-bold hover:opacity-90 transition">
             Start
         </a>
     </div>
