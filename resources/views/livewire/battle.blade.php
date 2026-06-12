e29636eda9eef61c2c196bb489961011bbc11014
IsmaelG <ismaelgn89@gmail.com>
2026-06-12 13:43:36 +0200

    feat: display current HP and MP for character and enemy

--- resources/views/livewire/battle.blade.php
+++ resources/views/livewire/battle.blade.php
@@ -24,8 +24,8 @@
 
         <div class="border-2 border-yellow-400 p-4">
             <h3 class="text-yellow-400 font-bold mb-2">{{ ucfirst($character->class) }}</h3>
-            <p class="text-yellow-400">HP: {{ $character->max_health_points }}</p>
-            <p class="text-yellow-400">MP: {{ $character->max_magic_points }}</p>
+            <p class="text-yellow-400">HP: {{ $characterCurrentHp}}</p>
+            <p class="text-yellow-400">MP: {{ $characterCurrentMp }}</p>
         </div>
 
         <div class="border-2 border-yellow-400 p-4">
@@ -35,7 +35,7 @@
         <div class="border-2 border-yellow-400 p-4">
             <h3 class="text-yellow-400 font-bold mb-2">{{ $enemy->enemy_name }}</h3>
             <p class="text-yellow-400">HP: {{ $enemyCurrentHp }}</p>
-            <p class="text-yellow-400">MP: {{ $enemy->max_magic_points }}</p>
+            <p class="text-yellow-400">MP: {{ $enemyCurrentMp }}</p>
         </div>
 
     </div>
