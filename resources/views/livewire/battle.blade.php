9fe6ab469eba4fa3272bcdf457e9fcd502a2236e
IsmaelG <ismaelgn89@gmail.com>
2026-06-15 19:59:41 +0200

    feat: add batlle log component in battle view

--- resources/views/livewire/battle.blade.php
+++ resources/views/livewire/battle.blade.php
@@ -43,12 +43,7 @@
             :characterCurrentHp="$characterCurrentHp"
             :characterCurrentMp="$characterCurrentMp"/>
 
-        <div class="border-2 border-yellow-400 p-4 h-32 overflow-y-auto" style="-ms-overflow-style: none; scrollbar-width: none;">
-            <h3 class="text-yellow-400 font-bold mb-2">Battle Log</h3>
-            @foreach($battleLog as $action)
-                <p class="text-yellow-400 text-sm">{{ $action }}</p>
-            @endforeach
-        </div>
+        <livewire:battle.battle-log :battleLog="$battleLog"/>
 
         <livewire:battle.battle-enemy-stats 
             :enemy="$enemy"
