0cbbc48b2ca582fc394ab48119bd9c5909895ac0
IsmaelG <ismaelgn89@gmail.com>
2026-06-18 23:34:56 +0200

    feat: add continue button on victory

--- resources/views/livewire/battle.blade.php
+++ resources/views/livewire/battle.blade.php
@@ -15,12 +15,20 @@
 
         <livewire:battle.battle-log :battleLog="$battleLog"/>
 
+        @if($battleResult === \App\Enums\BattleResult::Win->value)
+            <div class="flex items-center justify-center">
+                <button type="button" wire:click="goToBetweenBattles" class="w-full h-full bg-gradient-to-b from-yellow-300 to-red-800 text-blue-950 rounded-lg px-8 py-3 text-xl font-bold transition-all transform hover:scale-102">
+                    Continue
+                </button>
+            </div>
+        @else
         <livewire:battle.battle-enemy-stats 
             :enemyName="$enemy->enemy_name"
             :enemyCurrentHp="$enemyCurrentHp"
             :enemyCurrentMp="$enemyCurrentMp"
             :maxHp="$enemy->max_health_points"
             :maxMp="$enemy->max_magic_points"/>
+        @endif
 
     </div>
 </div>
