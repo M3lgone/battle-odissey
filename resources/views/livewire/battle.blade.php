92aa123543bce312b4064bef27741751d2671548
IsmaelG <ismaelgn89@gmail.com>
2026-06-20 22:42:41 +0200

    feat: add victory view and final boss logic

--- resources/views/livewire/battle.blade.php
+++ resources/views/livewire/battle.blade.php
@@ -2,6 +2,9 @@
 
     @if($battleResult === \App\Enums\BattleResult::Loss->value)
         <livewire:battle.battle-game-over />
+
+    @elseif($battleResult === \App\Enums\BattleResult::Win->value && $this->isFinalBoss())
+        <livewire:battle.battle-victory />
     @endif
 
     <livewire:battle.battle-scene 
@@ -24,18 +27,21 @@
         <livewire:battle.battle-log :battleLog="$battleLog"/>
 
         @if($battleResult === \App\Enums\BattleResult::Win->value)
+
+        @if(!$this->isFinalBoss())
             <div class="flex items-center justify-center">
                 <button type="button" wire:click="goToBetweenBattles" class="w-full h-full bg-gradient-to-b from-yellow-300 to-red-800 text-blue-950 rounded-lg px-8 py-3 text-xl font-bold transition-all transform hover:scale-102">
                     Continue
                 </button>
             </div>
+        
         @else
-        <livewire:battle.battle-enemy-stats 
-            :enemyName="$enemy->enemy_name"
-            :enemyCurrentHp="$enemyCurrentHp"
-            :enemyCurrentMp="$enemyCurrentMp"
-            :maxHp="$enemy->max_health_points"
-            :maxMp="$enemy->max_magic_points"/>
+            <livewire:battle.battle-enemy-stats 
+                :enemyName="$enemy->enemy_name"
+                :enemyCurrentHp="$enemyCurrentHp"
+                :enemyCurrentMp="$enemyCurrentMp"
+                :maxHp="$enemy->max_health_points"
+                :maxMp="$enemy->max_magic_points"/>
         @endif
 
     </div>
