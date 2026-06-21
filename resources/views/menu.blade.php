e2db97e729f05dfa6960f8425c7d23d2b34bcdc8
IsmaelG <ismaelgn89@gmail.com>
2026-06-21 11:15:18 +0200

    style: add border to menu, round to choose character and change text size

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,9 +1,13 @@
 <x-layouts.app>
     <div class="flex flex-col items-center justify-center min-h-screen gap-64">
-        <img src="{{ asset('images/logo/logo-battle-odissey.png') }}" alt="Battle Odyssey" class="w-[800px]">
+        
+        <div class="flex flex-col items-center justify-center border-4 border-yellow-400 rounded-2xl p-10 md:p-16 gap-20 shadow-2xl">
 
-        <a href="{{ route('character.select') }}" class="bg-gradient-to-b from-yellow-300 to-red-800 text-blue-950 rounded-lg px-8 py-3 text-xl font-bold transition-all transform hover:scale-105">
-            New Game
-        </a>
+            <img src="{{ asset('images/logo/logo-battle-odissey.png') }}" alt="Battle Odyssey" class="w-[800px]">
+
+            <a href="{{ route('character.select') }}" class="bg-gradient-to-b from-yellow-300 to-red-800 text-blue-950 rounded-lg px-8 py-3 text-xl font-bold transition-all transform hover:scale-105">
+                New Game
+            </a>
+        </div>
     </div>
 </x-layouts.app>
